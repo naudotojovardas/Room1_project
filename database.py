c6c76da5258cb7e838ba95374931121f9ec80cf0
Lukas Turskas <lukas.turskas@codeacademylt.onmicrosoft.com>
2024-10-01 17:38:14 +0300

    Update and rename database_models.py to database.py
    database.py file without models file. UPDATE

--- database.py
+++ database.py
@@ -21,28 +21,6 @@ Base = declarative_base()
 # test connection
 Base.metadata.create_all(bind=engine)
 
-# User model
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    todos = relationship("Todo", back_populates="owner")
-    # todos = Column(Integer, ForeignKey("todos.id"))
-
-# Todo model
-class Todo(Base):
-    __tablename__ = "todos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, index=True)
-    due_date = Column(String, index=True)
-    status = Column(Boolean, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="todos")
 
 
 
